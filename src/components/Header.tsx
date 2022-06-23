d455f6e793ec089f104040fb0032ea6e1176d2d3
crystian97 <icrystianveloso@gmail.com>
2022-06-23 11:29:14 -0300

    Criação do componente Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-export function Header(){
-    return(
-        <h1>Header</h1>
-    )
+import { Logo } from "./Logo";
+
+export function Header() {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <Logo />
+    </header>
+  );
 }
