f6fd3f6c2ed6ee603e4ffb62643c04c5b6214fd2
crystian97 <icrystianveloso@gmail.com>
2022-06-23 10:49:11 -0300

    Componetização dos elementos

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-const GET_LESSONS_QUERY = gql `
-query {
-  lessons{
-    id 
-    title
-    teacher{
-      name
-    }
-  }
-}
-`
+
 function App() {
-  // useEffect(()=>{
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then(response => {
-  //     console.log(response.data);
-  //   })
-  // },[])
-  const {data} = useQuery<{lessons:lesson[]}>(GET_LESSONS_QUERY)
-  console.log(data);
-  interface lesson{
-    id:string;
-    title:string;
-  }
+ 
   return (
-    <ul>
-      {data?.lessons.map(lesson=> {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+
+    </div>
   )
 }
 
